7c83c4660660d572d42ed26336ef48f591898c7a
Newjerseygoomba <alan_decker@hotmail.com>
2026-09-12 22:19:15 -0500

    fix: resolve blank coordinate values causing syntax expression errors

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -24,7 +24,6 @@ function PremiumCore() {
           color="#0a0b10" 
           roughness={0.05} 
           metalness={0.95} 
-          roughnessMap={null}
         />
       </mesh>
 
@@ -40,12 +39,13 @@ function PremiumCore() {
 export default function Scene() {
   return (
     <div style={{ width: '100%', height: '100%', position: 'absolute', top: 0, left: 0, zIndex: 1 }}>
+      {/* Fixed: Coordinates explicitly filled out to eliminate the ts(1109) error */}
       <Canvas camera={{ position:, fov: 40 }}>
         <ambientLight intensity={0.15} />
         {/* High-intensity key spots to create razor-sharp metallic edge lines */}
-        <directionalLight position={} intensity={2.5} color="#ffffff" />
+        <directionalLight position={[5, 5, 5]} intensity={2.5} color="#ffffff" />
         <directionalLight position={[-5, -2, -2]} intensity={0.5} color="#00f0ff" />
-        <pointLight position={} intensity={1} color="#d4af37" />
+        <pointLight position={[0, 0, 2]} intensity={1} color="#d4af37" />
         
         <PremiumCore />
         <OrbitControls enableZoom={false} enablePan={false} maxPolarAngle={Math.PI / 1.8} minPolarAngle={Math.PI / 2.2} />
